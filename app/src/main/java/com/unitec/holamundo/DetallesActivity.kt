d453fdf8d314f9ff052670960d33f380f58edced
daton <rapidclimate@gmail.com>
2021-08-06 18:36:16 -0500

    Viernes 6 de Agosto

--- app/src/main/java/com/unitec/holamundo/DetallesActivity.kt
+++ app/src/main/java/com/unitec/holamundo/DetallesActivity.kt
@@ -2,6 +2,7 @@ package com.unitec.holamundo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.Style
@@ -18,7 +19,24 @@ class DetallesActivity : AppCompatActivity() {
 
         var usuario=    intent.extras?.getSerializable("usuario") as Usuario
 
-        Toast.makeText(this, "registros ${usuario.localizacion?.size}", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "registros: ${usuario.localizacion?.size}", Toast.LENGTH_LONG).show()
+
+     var indiceMaximo=usuario.localizacion?.size
+
+        var ultimosCinco=ArrayList<Localizacion>()
+      //Vamos a iterar  o recorrer ese arreglo para que nos de los ultimos 5 registros
+      usuario.localizacion?.forEachIndexed { index, localizacion ->
+          if(index>=indiceMaximo?.minus(5)!!){
+              ultimosCinco.add(localizacion)
+             //Imprimimos en el Logcat ese elemento
+             Log.i("xx","Ultimos cinco ${localizacion.fecha.toString()} ")
+          }
+
+      }
+    //Aqui en esta parte del codigo lo unico ue tienen que hacer es mostrar
+// los marcadores con las ultimas 5 localizaciones del usuario seleccionado
+ // del arreglo ultimosCinco.
+
 
 
 
